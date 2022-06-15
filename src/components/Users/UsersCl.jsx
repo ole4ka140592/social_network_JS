b1dca9eb3b2128e8de099f7b499010656cd9ba80
ole4ka140592 <ole4ka140592@gmail.com>
2022-06-15 23:56:04 +0300

    add class component UsersCl

---
+++ src/components/Users/UsersCl.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import classes from "./Users.module.css";
+import axios from "axios";
+import photoUsers from "./../../assets/images/photoUsers.png"
+
+
+class UsersCL extends React.Component {
+
+    getUsers = () => {
+        if (this.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    debugger
+                    this.setUsers(response.data.items)
+                })
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+                <button onClick={this.getUsers}>GET USERS</button>
+                {this.props.users.map(m => {
+                    return (
+                        <div key={m.id}>
+                        <span>
+                            <div>
+                                <img className={classes.photo}
+                                     src={m.photos.small !== null ? m.photos.small : photoUsers}/>
+                            </div>
+                            <div>
+                                {m.followed
+                                    ? <button onClick={() => this.follow(m.id)}>unFollow</button>
+                                    : <button onClick={() => this.follow(m.id)}>Follow</button>}
+                            </div>
+                        </span>
+                            <span>
+                            <span>
+                                <div>{m.name}</div>
+                                <div>{m.status}</div>
+                            </span>
+                        </span>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
